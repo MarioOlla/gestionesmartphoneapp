b9576696932cc313cdebdcabaafb69bb4935c916
MarioOlla <mario.olla25@gmail.com>
2022-10-26 19:40:37 +0200

    Completata la definizione dei modelli

---
+++ src/main/java/it/prova/gestionesmartphoneapp/test/TestGestione.java
@@ -0,0 +1,16 @@
+package it.prova.gestionesmartphoneapp.test;
+
+public class TestGestione {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+//	Inserimento nuovo Smartphone
+//	Aggiornamento versioneOS di uno Smartphone esistente
+//	Inserimento nuova App
+//	Aggiornamento versione APP con relativa data
+//	Installazione di App esistente su Smartphone esistente
+//	Disinstallazione di una App da uno Smartphone
+//	Rimozione completa di uno Smartphone associato a due App
+}
